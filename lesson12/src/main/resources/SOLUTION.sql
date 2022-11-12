08d3a6e2ab08aaaf48ccf49fce97c8ec47b8d340
= <=>
2022-11-12 23:11:53 +0400

    Very interesting.

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-delete from student where not EXISTS(select id in (SELECT DISTINCT m.student_id FROM mark AS m JOIN subject AS s on s.id = m.subject_id WHERE grade >= 4));
+delete from student where id in (SELECT DISTINCT m.student_id FROM mark AS m JOIN subject AS s on s.id = m.subject_id WHERE grade >= 4);
 delete from STUDENT where ID in (select STUDENT_ID from MARK group by STUDENT_ID having min(MARK) < 4);
-DELETE FROM paymenttype WHERE not EXISTS(select name = 'DAILY');
+DELETE FROM paymenttype WHERE name = 'DAILY';
 delete from MARK where mark < 7;
